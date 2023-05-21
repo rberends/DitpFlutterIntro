1251933e7e2700fead1ea25d5d5111021f3fe1fd
rberends <kkndvulcanic@gmail.com>
2023-05-21 19:31:04 +0200

    Initial changes, add colors, start on top bar

---
+++ lib/topbar/background_clipper.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class BackgroundWaveClipper extends CustomClipper<Path> {
+
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0.0, size.height);
+    // We subtracted 80 from the height here
+    path.lineTo(size.width, size.height - 80);
+    path.lineTo(size.width, 0.0);
+    return path;
+  }
+  @override
+  bool shouldReclip(BackgroundWaveClipper oldClipper) =>
+      oldClipper != this;
+}
---
+++ lib/utils/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+const Color dPrimaryColor = Color.fromRGBO(147, 191, 25, 0.78);
+const Color dSecondaryColor = Color.fromRGBO(2, 48, 71, 1);
+const Color dBackgroundColor = Color.fromRGBO(255, 255, 255, 1);
+const Color dDangerColor = Color.fromRGBO(249, 77, 30, 1);
+const Color dCaptionColor = Color.fromRGBO(166, 177, 187, 1);
+
+// Lets replace all static sizes
+const double kDesktopMaxWidth = 1000.0;
+const double kTabletMaxWidth = 760.0;
+double getMobileMaxWidth(BuildContext context) =>
+    MediaQuery.of(context).size.width * .8;
